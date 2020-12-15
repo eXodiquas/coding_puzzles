ea2058c7a7b06642dcddba0ae459a5fb88af1dfa
Timo 'eXodiquas' Netzer <exodiquas@gmail.com>
2020-12-15 14:19:45 +0100

    removed timers and refactored

--- advent_of_code/2020/day_15/day_15.rkt
+++ advent_of_code/2020/day_15/day_15.rkt
@@ -1,7 +1,12 @@
 #lang racket
 ;;; INPUT
 
-(define input #hash((1 . 1) (0 . 2) (18 . 3) (10 . 4) (19 . 5) (6 . 6)))
+(define input #hash((1 . 1)
+                    (0 . 2)
+                    (18 . 3)
+                    (10 . 4)
+                    (19 . 5)
+                    (6 . 6)))
 
 ;;; DAY 15 - PART 1
 
@@ -14,8 +19,8 @@
             (aux (hash-set ht recent c) (sub1 n) (add1 c) (- c (hash-ref ht recent))))))
   (aux initial (- n (hash-count initial)) (add1 (hash-count initial)) 0))
 
-(time (generate-upto input 2020))
+(generate-upto input 2020)
 
 ;;; DAY 15 - PART 2 
 
-(time (generate-upto input 30000000))
+(generate-upto input 30000000)
